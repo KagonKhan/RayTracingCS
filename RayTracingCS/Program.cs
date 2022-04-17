b3548343a109c27af4ba58414dd00d4a4e6665ea
KagonKhan <s.szynk98@gmail.com>
2022-04-17 04:59:26 +0200

    Add project files.

---
+++ RayTracingCS/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RayTracingCS
+{
+    public struct Point
+    {
+        double x, y, z, w;
+
+        public Point(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = 1.0d;
+        }
+    }
+    public struct Vector
+    {
+        double x, y, z, w;
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = 0.0d;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
